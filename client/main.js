ae6566d110e59b1139114c64bb4221dcd6cec2c1
NedxLab <odoc047@gmail.com>
2023-01-12 01:22:11 +0100

    UPdating the Server Url

--- client/main.js
+++ client/main.js
@@ -62,7 +62,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-3-wxxd.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json  " },
     body: JSON.stringify({
